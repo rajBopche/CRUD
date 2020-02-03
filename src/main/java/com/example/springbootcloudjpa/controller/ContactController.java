1be705b10762cb1e9436f11310260fffd13f227c
rajbopche <raj.bopche@paynearby.in>
2020-02-03 18:26:36 +0530

    added a service layer between repo and controller

--- src/main/java/com/example/springbootcloudjpa/controller/ContactController.java
+++ src/main/java/com/example/springbootcloudjpa/controller/ContactController.java
@@ -3,67 +3,51 @@ package com.example.springbootcloudjpa.controller;
 import com.example.springbootcloudjpa.model.Contact;
 import com.example.springbootcloudjpa.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping({"/contacts"})
 public class ContactController {
 
-
-    //private ContactRepository repository;
     @Autowired
     private ContactService contactService;
 
-
-   /* ContactController(ContactRepository contactRepository) {
-        this.repository = contactRepository;
-    }
-
-    @GetMapping
-    public List findAll() {
-        return repository.findAll();
-    }
-
-    @GetMapping(path = {"/{id}"})
-    public ResponseEntity<Contact> findById(@PathVariable("id") long id) {
-        return repository.findById(id)
-                .map(record -> ResponseEntity.ok().body(record))
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @PostMapping
-    public Contact create(@RequestBody Contact contact) {
-
-        return repository.save(contact);
-    }
-
-    @PutMapping(path = {"/id"})
-    public ResponseEntity<Contact> update(@PathVariable("id") long id, @RequestBody Contact contact) {
-        return repository.findById(id)
-                .map(record -> {
-                    record.setName(contact.getName());
-                    record.setEmail(contact.getEmail());
-                    record.setPhone(contact.getPhone());
-                    Contact updatedContact = repository.save(record);
-                    return ResponseEntity.ok().body(updatedContact);
-                }).orElse(ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping(path = {"/id"})
-    public ResponseEntity<?> delete(@PathVariable("id") long id) {
-        return repository.findById(id)
-                .map(record -> {
-                    repository.deleteById(id);
-                    return ResponseEntity.ok().build();
-                }).orElse(ResponseEntity.notFound().build());
-    }*/
-
-    @RequestMapping(value = "/savecontact", method = RequestMethod.POST)
+    @PostMapping(value = "/savecontact")
     public Contact create(@RequestBody Contact contact) {
         return contactService.saveContact(contact);
     }
 
+    @PostMapping(value = "/saveallcontact")
+    public List<Contact> create(@RequestBody List<Contact> contact) {
+        return contactService.saveContact(contact);
+    }
+
+    @GetMapping(value = "/getallcontacts")
+    public List<Contact> findAll() {
+        return contactService.getAllContact();
+    }
+
+    @GetMapping(value = "/getcontactbyid/{id}")
+    public Optional<Contact> findContactById(@PathVariable("id") long id) {
+        return contactService.getContactById(id);
+    }
+
+    @DeleteMapping(value = "/deletecontactbyid/{id}")
+    public ResponseEntity<?> deleteContactById(@PathVariable("id") long id) {
+        return contactService.deleteContactById(id);
+    }
+
+    @DeleteMapping(value = "/deleteallcontactbyid")
+    public ResponseEntity<?> deleteAllContacts() {
+        return contactService.deleteAllContacts();
+    }
+
+    @PutMapping(value = "/updatecontactbyid/{id}")
+    public ResponseEntity<?> updateContact(@PathVariable("id") long id, @RequestBody Contact contact) {
+        return contactService.updateContactById(id, contact);
+    }
 }
--- src/main/java/com/example/springbootcloudjpa/service/ContactService.java
+++ src/main/java/com/example/springbootcloudjpa/service/ContactService.java
@@ -3,7 +3,12 @@ package com.example.springbootcloudjpa.service;
 import com.example.springbootcloudjpa.model.Contact;
 import com.example.springbootcloudjpa.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ContactService {
@@ -19,5 +24,63 @@ public class ContactService {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public List<Contact> saveContact(List<Contact> contact) {
+        try {
+            return contact.stream().map(record -> contactRepository.save(record)).collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public List<Contact> getAllContact() {
+        try {
+            return contactRepository.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Optional<Contact> getContactById(long id) {
+        try {
+            return contactRepository.findById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+    }
+
+    public ResponseEntity<?> deleteContactById(long id) {
+        try {
+            contactRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    public ResponseEntity<?> deleteAllContacts() {
+        try {
+            contactRepository.deleteAll();
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    public ResponseEntity<?> updateContactById(long id, Contact contact) {
+        return contactRepository.findById(id)
+                .map(record -> {
+                    record.setName(contact.getName());
+                    record.setEmail(contact.getEmail());
+                    record.setPhone(contact.getPhone());
+                    Contact updatedContact = contactRepository.save(record);
+                    return ResponseEntity.ok().body(updatedContact);
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
